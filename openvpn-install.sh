3b1e32507a06e559f4dead9af49d76e7f8ff94be
yangmengbin <wf0719001030@gmail.com>
2023-03-14 15:37:15 +0800

    server.conf set 'duplicate-cn'

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -714,6 +714,7 @@ crl-verify crl.pem" >> /etc/openvpn/server/server.conf
 	if [[ "$protocol" = "udp" ]]; then
 		echo "explicit-exit-notify" >> /etc/openvpn/server/server.conf
 	fi
+    echo "duplicate-cn" >> /etc/openvpn/server/server.conf
 	update_sysctl
 	if systemctl is-active --quiet firewalld.service; then
 		# Using both permanent and not permanent rules to avoid a firewalld
